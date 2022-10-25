0160b2f20c2c87e05e756c5b5e5439bc2b8b5c75
Sebastian Sassone <108636772+SebastianSassone@users.noreply.github.com>
2022-10-24 23:34:40 -0300

    Performance Commit

--- js/script.js
+++ js/script.js
@@ -37,13 +37,9 @@ button_mostrar_tabla.addEventListener('click', () => {
   tabla_de_notas.style.display = 'block'
   button_ocultar_tabla.style.display = 'block'
   button_mostrar_tabla.style.display = 'none'
- 
-
 })
 
-
-
-// Samples c4
+// Variables Samples c4
 
 let even_tecla_DO4 = document.getElementById('even_tecla_DO4');
 let even_tecla_RE4 = document.getElementById('even_tecla_RE4');
@@ -53,24 +49,26 @@ let even_tecla_SOL4 = document.getElementById('even_tecla_SOL4');
 let even_tecla_LA4 = document.getElementById('even_tecla_LA4');
 let even_tecla_SI4 = document.getElementById('even_tecla_SI4');
 
-//Samples c4 tecla negras
+// Variables Samples c4 tecla negras
+
 let tecla_negraD04 = document.getElementById('tecla_negraD04');
 let tecla_negraRE4 = document.getElementById('tecla_negraRE4');
 let tecla_negraFA4 = document.getElementById('tecla_negraFA4');
 let tecla_negraSOL4 = document.getElementById('tecla_negraSOL4');
 let tecla_negraLA4 = document.getElementById('tecla_negraLA4');
 
-// Samples c5
+// Variables Samples c5
 
 let even_tecla_DO5 = document.getElementById('even_tecla_DO5');
 let even_tecla_RE5 = document.getElementById('even_tecla_RE5');
 let even_tecla_MI5 = document.getElementById('even_tecla_MI5');
 
-// Samples c5 teclas negras 
+// Variables Samples c5 teclas negras 
+
 let tecla_negraDO5 = document.getElementById('tecla_negraDO5');
 let tecla_negraRE5 = document.getElementById('tecla_negraRE5');
 
-// Samples c4
+// Evento click Samples c4
 even_tecla_DO4.addEventListener('click', () => {
   let tecla = new Audio('song/Doc4.mp3')
   tecla.play();
@@ -101,7 +99,7 @@ even_tecla_SI4.addEventListener('click', () => {
   tecla.play()
 })
 
-//Samples c4 teclas negras
+// Evento click Samples c4 teclas negras
 
 tecla_negraD04.addEventListener('click', () => {
   let tecla = new Audio('song/DoN.mp3')
@@ -124,7 +122,7 @@ tecla_negraLA4.addEventListener('click', () => {
   tecla.play(); 
 })
 
-// Samples c5
+// Evento click Samples c5
 
 even_tecla_DO5.addEventListener('click', () => {
   let tecla = new Audio('song/Doc5.mp3')
@@ -139,7 +137,7 @@ even_tecla_MI5.addEventListener('click', () => {
   tecla.play()
 } )
 
-//Samples c5 teclas negras
+// Evento click Samples c5 teclas negras
 
 tecla_negraDO5.addEventListener('click', () => {
   tecla = new Audio('song/DoNc5.mp3')
@@ -150,134 +148,159 @@ tecla_negraRE5.addEventListener('click', () => {
   tecla.play() 
 }  )
 
-// Samples c4 
+// Evento keydown  Samples c4 
 
 window.addEventListener('keydown', (e) =>{ 
   let teclado = e.key
-  if(teclado == 'q'|| teclado == 'Q'){ 
-let tecla = new Audio('song/Doc4.mp3')
-  tecla.play()};
+  if(teclado == 'q'|| teclado == 'Q'){
+    modificarTeclasBlancas(0) 
+    let tecla = new Audio('song/Doc4.mp3')
+    tecla.play()};
 })
 window.addEventListener('keydown', (e) => { 
   let teclado = e.key
   if(teclado == 'w'|| teclado == 'W'){
-  let tecla = new Audio('song/Rec4.mp3')
-  tecla.play();}
+    modificarTeclasBlancas(1)
+    let tecla = new Audio('song/Rec4.mp3')
+    tecla.play();}
 })
 window.addEventListener('keydown', (e) => { 
   let teclado = e.key
   if(teclado == 'e'||teclado == 'E'){
-  let tecla = new Audio('song/Mic4.mp3')
-  tecla.play()}
+    modificarTeclasBlancas(2)
+    let tecla = new Audio('song/Mic4.mp3')
+    tecla.play()}
 })
 window.addEventListener('keydown', (e) => { 
   let teclado = e.key
   if(teclado == 'r'|| teclado == 'R'){
-  let tecla = new Audio('song/Fac4.mp3')
-  tecla.play()}
+    modificarTeclasBlancas(3)
+    let tecla = new Audio('song/Fac4.mp3')
+    tecla.play()}
 })
 window.addEventListener('keydown', (e) => { 
   let teclado = e.key
   if(teclado == 't'|| teclado == 'T'){
-  let tecla = new Audio('song/Solc4.mp3')
-  tecla.play()}
+    modificarTeclasBlancas(4)
+    let tecla = new Audio('song/Solc4.mp3')
+    tecla.play()}
 })
 window.addEventListener('keydown', (e) => { 
   let teclado = e.key
   if(teclado ==  'y' || teclado == 'Y'){
-  let tecla = new Audio('song/Lac4.mp3')
-  tecla.play()}
+    modificarTeclasBlancas(5)
+    let tecla = new Audio('song/Lac4.mp3')
+    tecla.play()}
 })
 window.addEventListener('keydown', (e) => { 
   let teclado = e.key
   if(teclado ==  'u'||teclado == 'U'){
-  let tecla = new Audio('song/Sic4.mp3')
-  tecla.play()}
+    modificarTeclasBlancas(6)
+    let tecla = new Audio('song/Sic4.mp3')
+    tecla.play()}
 })
 
-//Samples c4 teclas negras
+// Evento keydown Samples c4 teclas negras
 
-window.addEventListener('keypress', (e) => {
+window.addEventListener('keydown', (e) => {
   let teclado = e.key 
   if (teclado == 2){
-  let tecla = new Audio('song/DoN.mp3')
-  tecla.play();
+    modificarTeclasNegras(0)
+    let tecla = new Audio('song/DoN.mp3')
+    tecla.play();
   }
 })
-window.addEventListener('keypress', (e) => {
+window.addEventListener('keydown', (e) => {
   let teclado = e.key 
   if (teclado == 3){
+    modificarTeclasNegras(1)
   let tecla = new Audio('song/ReN.mp3')
   tecla.play();
   }
 })
-window.addEventListener('keypress', (e) => {
+window.addEventListener('keydown', (e) => {
   let teclado = e.key 
   if (teclado == 5){
-  let tecla = new Audio('song/FaN.mp3')
-  tecla.play();
+    modificarTeclasNegras(2)
+    let tecla = new Audio('song/FaN.mp3')
+    tecla.play();
   }
 })
-window.addEventListener('keypress', (e) => {
+window.addEventListener('keydown', (e) => {
   let teclado = e.key 
   if (teclado == 6){
-  let tecla = new Audio('song/SolN.mp3')
-  tecla.play();
+    modificarTeclasNegras(3)
+    let tecla = new Audio('song/SolN.mp3')
+    tecla.play();
   }
 })
-window.addEventListener('keypress', (e) => {
+window.addEventListener('keydown', (e) => {
   let teclado = e.key 
   if (teclado == 7){
-  let tecla = new Audio('song/LaN.mp3')
-  tecla.play();
+    modificarTeclasNegras(4)
+    let tecla = new Audio('song/LaN.mp3')
+    tecla.play();
   }
 })
 
-// Samples c5
+// Evento keydown  Samples c5
 
 window.addEventListener('keydown', (e) => {
   let teclado = e.key 
   if(teclado == 'i'|| teclado == 'I'){   
-  let tecla = new Audio('song/Doc5.mp3')
-  tecla.play()
+    modificarTeclasBlancas(7)
+    let tecla = new Audio('song/Doc5.mp3')
+    tecla.play()
   }
 })
-
 window.addEventListener('keydown', (e) => {
   let teclado = e.key 
   if(teclado == 'o'|| teclado == 'O'){     
-  let tecla = new Audio('song/ReC5.mp3')
-  tecla.play()
+    modificarTeclasBlancas(8)
+    let tecla = new Audio('song/ReC5.mp3')
+    tecla.play()
   }
 })
-
 window.addEventListener('keydown', (e) => {
   let teclado = e.key 
   if(teclado == 'p'|| teclado == 'P'){     
-   let tecla = new Audio('song/Mic5.mp3')
-  tecla.play() 
+    modificarTeclasBlancas(9)
+    let tecla = new Audio('song/Mic5.mp3')
+    tecla.play() 
   }
 })
 
-//Samples c5 teclas negras
+// Evento keydown Samples c5 teclas negras
 
-window.addEventListener('keypress', (e) => {
+window.addEventListener('keydown', (e) => {
   let teclado = e.key
   if(teclado == 9){
+    modificarTeclasNegras(5)
     let tecla = new Audio('song/DoNc5.mp3')
     tecla.play() 
   }
 })
-window.addEventListener('keypress', (e) => {
+window.addEventListener('keydown', (e) => {
   let teclado = e.key
   if(teclado == 0){
+    modificarTeclasNegras(6)
     let tecla = new Audio('song/ReNc5.mp3')
     tecla.play()
   }
 })
 
-let aptreta = document.getElementById('even_tecla_RE4').addEventListener('keydonw', () => {
-  aptreta.classlist.add('teclas_negras:hover')
-})
+// Animacion evento keydown Teclas Blancas
 
+function modificarTeclasBlancas(i){ 
+    let teclas_blancas = document.querySelectorAll('.tecla');
+    teclas_blancas[i].classList.add('teclas_blancas_ev');
+    setTimeout( () => {teclas_blancas[i].classList.remove('teclas_blancas_ev')}, 220);             
+}
 
+// Animacion evento keydown Teclas Negras
+
+function modificarTeclasNegras(i){ 
+    let teclas_blancas = document.querySelectorAll('.teclas_negras');
+    teclas_blancas[i].classList.add('teclas_negras_ev')
+    setTimeout( () => {teclas_blancas[i].classList.remove('teclas_negras_ev')}, 220)        
+}
